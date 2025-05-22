38753bc9e6f3e739f4dfcd7a75a0a3b388c56338
jose Angel <josegallardo.cor@gmail.com>
2025-05-22 00:46:38 -0600

    Agregar archivos de proyecto.

---
+++ Reports/IReportSaver.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// Interfaz para guardar reportes a partir de un metodo específico.
+namespace PracitcaSOLID.Reports
+{
+    public interface IReportSaver
+    {
+        void Save(string content);
+    }
+}
---
+++ Reports/IReportService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracitcaSOLID.Reports
+{
+    public interface IReportService
+    {
+        void RunReport(List<string> data);
+    }
+}
